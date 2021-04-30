859d06ff1186c83d41ba1d6627dfceb40a6402e9
Denis <denvys5@gmail.com>
2021-04-30 16:47:59 +0300

    Comments

--- TicTacToePackage/Input.java
+++ TicTacToePackage/Input.java
@@ -11,6 +11,7 @@ public class Input {
 
     public String getAnswer() throws IOException {
         String answer = bufferedReader.readLine();
+        //Вылеление памяти на новый экземлпяр класса при каждом вызове метода
         PrintInfo printInfo = new PrintInfo();
         while (!checkAnswer(answer)){
             printInfo.wrongInput(PrintInfo.WrongEnters.WRONG_ENTER_ANSWER);
@@ -23,6 +24,7 @@ public class Input {
     }
     public static int getSize() throws IOException {
         String s = bufferedReader.readLine();
+        //Вылеление памяти на новый экземлпяр класса при каждом вызове метода
         PrintInfo printInfo = new PrintInfo();
         while (!Pattern.matches("^[1-9]\\d*$",s)){
             printInfo.wrongInput(PrintInfo.WrongEnters.WRONG_ENTER_SIZE);
@@ -31,6 +33,7 @@ public class Input {
         return Integer.parseInt(s);
     }
     public int getRow() throws IOException {
+        //Вылеление памяти на новый экземлпяр класса при каждом вызове метода
         PrintInfo printInfo = new PrintInfo();
         printInfo.row();
         String s = bufferedReader.readLine();
@@ -40,6 +43,7 @@ public class Input {
         return Integer.parseInt(s);
     }
     public int getColumn() throws IOException {
+        //Вылеление памяти на новый экземлпяр класса при каждом вызове метода
         PrintInfo printInfo = new PrintInfo();
         printInfo.column();
         String s = bufferedReader.readLine();
