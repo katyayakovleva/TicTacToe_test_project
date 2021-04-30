b9351d9101d298463b5a6b458c01f8371d27b049
katyayakovleva <51397341+katyayakovleva@users.noreply.github.com>
2021-04-30 23:49:47 +0300

    Add files via upload

---
+++ java/TicTacToePackage/Input.java
@@ -0,0 +1,48 @@
+package TicTacToePackage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class Input {
+
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static PrintInfo printInfo = new PrintInfo();
+
+    public String getAnswer() throws IOException {
+        String answer = bufferedReader.readLine();
+        while (!checkAnswer(answer)){
+            printInfo.wrongInput(PrintInfo.WrongEnters.WRONG_ENTER_ANSWER);
+            answer = bufferedReader.readLine();
+        }
+        return answer;
+    }
+    private boolean checkAnswer(String answer){
+        return answer.equals("no") || answer.equals("yes");
+    }
+    public static int getSize() throws IOException {
+        String s = bufferedReader.readLine();
+        while (!Pattern.matches("^[1-9]\\d*$",s)){
+            printInfo.wrongInput(PrintInfo.WrongEnters.WRONG_ENTER_SIZE);
+            s = bufferedReader.readLine();
+        }
+        return Integer.parseInt(s);
+    }
+    public int getRow() throws IOException {
+        printInfo.row();
+        String s = bufferedReader.readLine();
+        while (!Pattern.matches("\\d",s)){
+            s = bufferedReader.readLine();
+        }
+        return Integer.parseInt(s);
+    }
+    public int getColumn() throws IOException {
+        printInfo.column();
+        String s = bufferedReader.readLine();
+        while (!Pattern.matches("\\d",s)){
+            s = bufferedReader.readLine();
+        }
+        return Integer.parseInt(s);
+    }
+}
